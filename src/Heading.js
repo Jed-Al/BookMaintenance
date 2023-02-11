5511ed439c0a06430180c26e8ff6b797d27351b3
Jed <prernakumari480@gmail.com>
2023-02-11 13:02:33 +0530

    added heading

---
+++ src/Heading.js
@@ -0,0 +1,12 @@
+import './Heading.css';
+
+function Heading() {
+  return (
+    <div className="Heading" >
+        <h2 className="title">BooksList Maintenance Form</h2>
+        <h5 className="subtitle">Add and view your books using local storage</h5>
+    </div>
+  );
+}
+
+export default Heading;
